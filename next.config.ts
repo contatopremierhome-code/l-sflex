46895ad35310564ed0dc01de7b8b4493957dc063
Premier Home <contatopremierhome@gmail.com>
2025-10-15 01:23:54 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -41,7 +41,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,"',
+        protocol: 'https',
         hostname: 'http2.mlstatic.com',
         port: '',
         pathname: '/**',
