4ff5d0022f7ddfdcfb6e1cc3b085b9ceecd1f7bf
Premier Home <contatopremierhome@gmail.com>
2025-10-15 02:18:40 +0000

    troque essa imagem por essa: "https://i.imgur.com/6cLZ6an.png" (_for ele

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
         <div className="flex justify-center">
           {heroImage && (
             <Image
-              src="https://s2-techtudo.glbimg.com/fEQaGUxtX2k6y5CgZJ1oYxvPDMs=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2024/n/A/1GeIM7ReyhPjlLIW7ijA/entregador-mercado-livre-2.jpg"
+              src="https://i.imgur.com/6cLZ6an.png"
               alt={heroImage.description}
               data-ai-hint={heroImage.imageHint}
               width={600}
