195de367f6a589e31aa5bba0c050aa5d507fe770
Premier Home <contatopremierhome@gmail.com>
2025-10-15 01:26:27 +0000

    você não colocou a imagem que eu pedi (agora vão ser 4 imagens e não 3)

--- src/components/sections/image-gallery.tsx
+++ src/components/sections/image-gallery.tsx
@@ -13,13 +13,17 @@ const images = [
     src: 'https://http2.mlstatic.com/frontend-assets/growth-melinet-front-end/proposal_operational_loads_pt.svg',
     alt: 'Ilustração de logística do Mercado Livre',
   },
+  {
+    src: 'https://magis5.com.br/wp-content/uploads/2022/03/Magis5-se-torna-integrador-diamante-no-projeto-acelera-magalu-do-magazine-luiza-1024x576.png',
+    alt: 'Integração com Magalu',
+  },
 ];
 
 export default function ImageGallery() {
   return (
     <section className="bg-card py-12 lg:py-24">
       <div className="container">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
           {images.map((image, index) => (
             <div key={index} className="overflow-hidden rounded-lg shadow-lg">
               <Image
